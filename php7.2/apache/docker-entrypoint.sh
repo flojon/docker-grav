42557dd67e223e6ea38ff6803363024415ac67dd
Jonas Flodén <jonas@floden.nu>
2018-08-06 22:13:52 +0200

    Compare Grav version on startup and upgrade if necessary

--- php7.2/apache/docker-entrypoint.sh
+++ php7.2/apache/docker-entrypoint.sh
@@ -1,7 +1,21 @@
 #!/bin/sh
 set -e
 
+message=""
+
 if [ -z "$(ls -A /var/www/html)" ]; then
+	message="Grav not found in $PWD - copying now..."
+else
+	# trick sort -V to handle -alpha, -beta, -rc etc. See https://stackoverflow.com/a/40391207
+	newest_version=`sed -n "s/^define('GRAV_VERSION', '\(.*\)');$/\1/p" system/defines.php /usr/src/grav-admin/system/defines.php | sed '/-/!{s/$/_/}' | sort -V | tail -1`
+	current_version=`sed -n "s/^define('GRAV_VERSION', '\(.*\)');$/\1/p" system/defines.php | sed '/-/!{s/$/_/}'`
+	if [ $newest_version != $current_version ]; then
+		message="Newer version of Grav found - copying now..."
+	fi
+fi
+
+if [ -n "$message" ]; then
+	echo $message
 	cp -a /usr/src/grav-admin/. /var/www/html/
 	chown -R www-data:www-data /var/www/html
 	if [ "$GRAV_REVERSE_PROXY" = "true" ]; then
